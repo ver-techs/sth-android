0adad17f0fcb92da670671f6337c7283d65aaa92
Aakifah Rahman <aakifahrahman@gmail.com>
2016-04-28 23:52:59 +0530

    Add moving progress to progress bar in landing page

--- app/src/main/java/com/ver_techs/sth_android/LandingActivity.java
+++ app/src/main/java/com/ver_techs/sth_android/LandingActivity.java
@@ -9,9 +9,12 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class LandingActivity extends AppCompatActivity {
 
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,43 @@ public class LandingActivity extends AppCompatActivity {
         //set layout
         setContentView(R.layout.activity_landing);
 
+        final ProgressBar pb = (ProgressBar) findViewById(R.id.pb);
+
+        // Set the progress status zero
+        progressStatus = 0;
+
+        /*
+                    A Thread is a concurrent unit of execution. It has its own call stack for
+                    methods being invoked, their arguments and local variables. Each application
+                    has at least one thread running when it is started, the main thread,
+                    in the main ThreadGroup. The runtime keeps its own threads
+                    in the system thread group.
+        */
+        // Start the lengthy operation in a background thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus < 100) {
+                    // Update the progress status
+                    progressStatus += 1;
+
+                    // Try to sleep the thread for 20 milliseconds
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Update the progress bar
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            pb.setProgress(progressStatus);
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 
 }
