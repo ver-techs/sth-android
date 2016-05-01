91714305594b60c8226857fbee25a412117585c1
Aakifah Rahman <aakifahrahman@gmail.com>
2016-05-01 18:39:02 +0530

    Add login check to fb callbackmanager

--- app/src/main/java/com/ver_techs/sth_android/MainActivity.java
+++ app/src/main/java/com/ver_techs/sth_android/MainActivity.java
@@ -1,7 +1,17 @@
 package com.ver_techs.sth_android;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by aakif on 01-05-2016.
  */
-public class MainActivity {
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
 }
